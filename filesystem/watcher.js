0a7b04942a3de62e5e9b8b23b77990617a61d25a
Scott Phelps <sphelps@millennialmedia.com>
2015-12-07 16:15:27 -0500

    spawn a child process

--- filesystem/watcher.js
+++ filesystem/watcher.js
@@ -1,12 +1,17 @@
+"use strict";
+
 const
   fs = require('fs'),
-  filename = process.argv[2];
+  filename = process.argv[2],
+  spawn = require('child_process').spawn;
+
 if (!filename) {
   throw Error("A file to watch must be specified!");
 }
 
 fs.watch(filename, function() {
-  console.log("File " + filename + " just changed!");
+  let ls = spawn('ls', ['-lh', filename]);
+  ls.stdout.pipe(process.stdout);
 });
 
 console.log("Now watching " + filename + " for changes...");
